bb48139a772e9fb61ed98e6d29f169de908bbcea
JulioBendz <bengutjul2@gmail.com>
2023-10-24 02:23:17 -0500

    CORRECIONES GENERALES

--- src/controllers/colors.c.js
+++ src/controllers/colors.c.js
@@ -56,20 +56,23 @@ export const createColors = async (req, res) => {
 
 export const deleteColors = async(req, res) => {
     try {
-
         const [result] = await pool.query('DELETE FROM colors WHERE id = ?', [req.params.id])
-    if (result.affectedRows <=0) return res.status(404).json({
-        message: 'Color not found'
-    })
 
-    res.sendStatus(204)
+        if (result.affectedRows <=0) {
+            return res.status(404).json({
+                message: 'Color not found'
+            });
+        } 
+
+        return res.status(204).json({
+            message: 'Deletion completed successfully'
+        });
 
     } catch (error){
         return res.status(500).json({
             message:'Something goes wrong'
-        })
+        });
     }
-    
 }
 
 export const updateColors = async (req, res) => {
