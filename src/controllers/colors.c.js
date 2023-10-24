faad939e6d11ae149e3ead6f9eaf39645a90875a
JulioBendz <bengutjul2@gmail.com>
2023-10-23 21:44:14 -0500

    rows corregido

--- src/controllers/colors.c.js
+++ src/controllers/colors.c.js
@@ -6,7 +6,7 @@ export const  getColors = async (req, res) => {
     try{
 
         const [rows] = await pool.query('SELECT  * FROM colors')
-        res.json(rows[0])
+        res.json(rows)
     } catch (error){
         return res.status(500).json({
             message:'Something goes wrong'
